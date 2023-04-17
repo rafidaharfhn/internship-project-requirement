45a4f235e1403c5f9c0453cf9c06cdc475e57c7b
Rafidah Arafahni Miladi <63544580+rafidaharfhn@users.noreply.github.com>
2023-04-17 14:17:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".toggle-password").click(function() {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+});
+
+
+
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled', $(this).scrollTop()>30);
+});
